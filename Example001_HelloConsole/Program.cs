42e6c55f4575430d8d6e9a9bd26a113226f7c651
Stepanov Nikolay <profy76794@gamil.com>
2023-11-30 18:23:59 +0300

    add HelloConsole

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Hello, World!"); // WriteLine - В конце перейти на новую строчку
+// dotnet new console - создать новый проект
+// dotnet run - запустить проект
+// Через "Console" мы обращаемся к системному терминалу

fbc5d15cd2d3f18fb096deb44161330627c144ad
Stepanov Nikolay <profy76794@gamil.com>
2023-11-30 18:58:45 +0300

    add Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,5 @@
+﻿double numberA = 12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
+// int - сложение, вычитание, умножение, деление целых чисел
+// double - сложение, вычитание, умножение, деление с остатком

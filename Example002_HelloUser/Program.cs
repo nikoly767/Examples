c7b550fff7c9aad75b1a180c2a04c9b947a640db
Stepanov Nikolay <profy76794@gamil.com>
2023-11-30 18:38:09 +0300

    add HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите ваше имя ");
+//Console.ReadLine(); - Считать строку из терминала
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+// Console.Write(); - Вывод в одну строку 
